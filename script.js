61cdd9d4a2397d11a4545dba2f1a38aa13b40a05
prithviraj-1657 <prithviraj.nst@gmail.com>
2026-04-01 21:07:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const API_KEY = "8167ac8c"; 
+const API_KEY = "8167ac8c";
 
 async function searchMovie() {
     const query = document.getElementById("searchInput").value;
-
     const moviesDiv = document.getElementById("movies");
-    moviesDiv.innerHTML = "Loading...";
+
+    if (!query) {
+        moviesDiv.innerHTML = "Please enter a movie name";
+        return;
+    }
+
+    moviesDiv.innerHTML = "<h2>Loading movies...</h2>";
 
     try {
         const response = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`);
         const data = await response.json();
+
+        if (data.Response === "False") {
+            moviesDiv.innerHTML = data.Error;
+            return;
+        }
 
         displayMovies(data.Search);
     } catch (error) {
@@ -21,12 +31,16 @@ function displayMovies(movies) {
     moviesDiv.innerHTML = "";
 
     movies.forEach(movie => {
+        const poster = movie.Poster !== "N/A"
+            ? movie.Poster
+            : "https://via.placeholder.com/150";
+
         const div = document.createElement("div");
         div.className = "movie";
 
         div.innerHTML = `
+            <img src="${poster}">
             <h3>${movie.Title}</h3>
-            <img src="${movie.Poster}" width="150">
             <p>${movie.Year}</p>
         `;
 
